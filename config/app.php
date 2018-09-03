7e61c2feca93e2626e26838d0f437cd9a0a8ae7b
vagrant <vagrant@homestead>
2018-09-03 09:02:12 +0000

    test X≈X

--- config/app.php
+++ config/app.php
@@ -37,6 +37,8 @@ return [
     | stack traces will be shown on every error that occurs within your
     | application. If disabled, a simple generic error page is shown.
     |
+    |
+    |
     */
 
     'debug' => env('APP_DEBUG', true),
